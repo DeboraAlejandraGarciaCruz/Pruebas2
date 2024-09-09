29603d3c4351513adb278ca150db5fa4199d32e6
Debora Alejandra Garcia Cruz <deboraalejandragarciacruz@gmail.com>
2024-09-08 23:28:29 -0600

    Create global.php

---
+++ global.php
@@ -0,0 +1,15 @@
+<?php 
+
+      require_once ("lib/carrito.class.php"); 
+      session_start(); 
+       
+      function do_sql ($sql) { 
+         mysql_connect ("localhost","root",""); 
+         mysql_select_db ("carrito"); 
+          
+         $res = mysql_query ($sql) or die ("SQL incorrecto : $sql"); 
+         return $res;       
+      } 
+       
+       
+?> 
