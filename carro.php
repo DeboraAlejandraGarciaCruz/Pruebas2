f98c3f003930268f0f0d74268f96ec96cc067fdf
Debora Alejandra Garcia Cruz <deboraalejandragarciacruz@gmail.com>
2024-09-08 23:27:39 -0600

    Create carro.php

---
+++ carro.php
@@ -0,0 +1,52 @@
+<?php 
+
+   require_once ("global.php"); 
+?> 
+
+<h3>Carrito de compras!
+  <?php    
+
+   /* entra a la categoria seleccionada */ 
+    
+   if (isset($_GET['cat']) && is_numeric($_GET['cat'])) { 
+      if (isset($_GET['item']) && is_numeric($_GET['item'])) { 
+       
+         $sql = "SELECT * FROM items WHERE id_item = ".$_GET['item']; 
+        $res = do_sql ($sql); 
+        $row = mysql_fetch_array ($res); 
+       
+         echo "<form method=\"post\" action=\"carrito.php\"> 
+                  <table width=\"58%\" height=\"146\" border=\"1\"> 
+                    <tr> 
+                      <td width=\"43%\" height=\"100\">&nbsp;imagen</td> 
+                      <td width=\"57%\" valign=\"top\"><h4>".$row['descripcion']."</h4></td> 
+                    </tr> 
+                    <tr> 
+                      <td height=\"38\" align=\"center\"><h6>".$row['nombre']."<br>$".$row['precio']."</h6></td> 
+                      <td>Cantidad : <input type=\"text\" name=\"cantidad\" size=\"3\" value=\"1\"> 
+                         <input type=\"hidden\" name=\"id_item\" value=\"".$row['id_item']."\"> 
+                         <input type=\"submit\" value=\"Agregar\"> 
+                        </td> 
+                    </tr> 
+                  </table> 
+                  </form>"; 
+         exit; 
+      } 
+       
+      $sql = "SELECT id_item,nombre,id_cat FROM items WHERE id_cat = ".$_GET['cat']." ORDER BY nombre"; 
+      $res = do_sql ($sql); 
+       
+      if (!mysql_num_rows($res)) { 
+         echo "No hay items en esta categor�a!"; 
+      } else {       
+         echo "<ul>"; 
+         while ($row = mysql_fetch_array($res)) { 
+       
+            echo "<li><b><a href=\"carro.php?cat=".$row['id_cat']."&item=".$row['id_item']."\">".$row['nombre']."</a></b></li> 
+                     <br><br>"; 
+         } 
+         echo "</ul>"; 
+      }    
+       
+?>
+
